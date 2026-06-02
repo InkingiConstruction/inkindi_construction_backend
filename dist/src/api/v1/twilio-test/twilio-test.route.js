f27e0fd17670f1ddbb1fd7bd876905c7a5d457df
Francis-250 <munyankindif0@gmail.com>
2026-06-02 10:41:50 +0200

    Make Twilio test route public temporarily

--- dist/src/api/v1/twilio-test/twilio-test.route.js
+++ dist/src/api/v1/twilio-test/twilio-test.route.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const role_middleware_1 = require("../middleware/role.middleware");
 const twilio_test_controller_1 = require("./twilio-test.controller");
 const router = (0, express_1.Router)();
-router.post("/send", auth_middleware_1.requiredAuth, role_middleware_1.isAdmin, twilio_test_controller_1.sendTwilioTestMessage);
+router.post("/send", twilio_test_controller_1.sendTwilioTestMessage);
 exports.default = router;
--- src/api/v1/twilio-test/twilio-test.route.ts
+++ src/api/v1/twilio-test/twilio-test.route.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { requiredAuth } from "../middleware/auth.middleware";
-import { isAdmin } from "../middleware/role.middleware";
 import { sendTwilioTestMessage } from "./twilio-test.controller";
 
 const router = Router();
 
-router.post("/send", requiredAuth, isAdmin, sendTwilioTestMessage);
+router.post("/send", sendTwilioTestMessage);
 
 export default router;
